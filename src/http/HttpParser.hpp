4459d802a385d617ed419682885899da0bbf32a9
Rami Mhazres <rmhazres@f1r5s16.codam.nl>
2026-05-21 17:00:00 +0200

    channged static functions to private

--- src/http/HttpParser.hpp
+++ src/http/HttpParser.hpp
@@ -6,7 +6,7 @@
 /*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/19 13:23:49 by rmhazres          #+#    #+#             */
-/*   Updated: 2026/05/21 11:30:27 by rmhazres         ###   ########.fr       */
+/*   Updated: 2026/05/21 16:58:36 by rmhazres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ class HttpParser
 				HttpParser& operator=(const HttpParser &other) = delete;
 [[nodiscard]]	HttpRequest parseHttp(const std::string &rawRequest) const;
 
+	private:
 				static void extractFirstLine(const std::string &line, HttpRequest &req);
 				static void extractHeaders(const std::string &line, HttpRequest &req);
-	private:
 		
 };
 
