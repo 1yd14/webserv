b45246a75553243dda80b1dd214bf8829d61a8f8
Lydia <159826376+1yd14@users.noreply.github.com>
2026-05-27 16:17:55 +0200

    Config file parser (#8)
    * made the datastructure of the ServerConfig parser, also added a test config file
    
    * removed directory include and added directory Config, to make the structure more clear
    
    * Server and Path are now seperate classes, those are instantiated in the config parser. Server is now parsed. No checks yet.
    
    * added parser for the location blocks
    
    * fixed a typo in ConfigParser
    
    * added exceptions in server parsing
    
    * fixed logic error in LocationBlock setMethods function
    
    * renamed main.cpp to test_config_parser.cpp
    
    * moved the test_config_parser.cpp into tests directory

---
+++ src/Config/Config.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Config.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/19 15:59:03 by lyvan-de          #+#    #+#             */
+/*   Updated: 2026/05/21 14:40:33 by lyvan-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Config.hpp"
+#include "ConfigParser.hpp"
+#include "Server.hpp"
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+Config::Config(std::string filename) {
+	std::ifstream file(filename);
+	if (!file) {
+		//handle error
+	}
+	std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	std::istringstream stream(content);
+	_servers = ConfigParser::parseConfig(stream);
+}
+
+std::vector<Server> Config::getServers() {
+	return _servers;
+}
---
+++ src/Config/Server.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Server.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyvan-de <lyvan-de@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/07 12:34:17 by lyvan-de          #+#    #+#             */
+/*   Updated: 2026/05/26 15:12:13 by lyvan-de         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <cstddef>
+#include <map>
+#include <vector>
+#include <string>
+#include "LocationBlock.hpp"
+
+
+class Server {
+	public:
+	int getPort();
+	std::string	getHost();
+	std::string getRoot();
+	std::string getIndex();
+	size_t getMaxBodySize();
+	std::map<int, std::string> getErrorPages();
+	std::vector<LocationBlock> getLocationBlocks();
+	void setPort(std::vector<std::string> values);
+	void setHost(std::vector<std::string> values);
+	void setRoot(std::vector<std::string> values);
+	void setIndex(std::vector<std::string> values);
+	void setMaxBodySize(std::vector<std::string> values);
+	void setErrorPages(std::vector<std::string> values);
+	void addLocation(LocationBlock location);
+
+	private:
+	int							_port;
+	std::string					_host;
+	std::string					_root;
+	std::string					_index;
+	size_t						_max_body_size;
+	std::map<int, std::string>	_error_pages;
+	std::vector<LocationBlock>	_location_blocks;
+};
+
