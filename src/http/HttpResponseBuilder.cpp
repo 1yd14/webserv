8dca34c38fc3f7c1e075485cc93c6de113c9e70e
Rami Mhazres <rmhazres@f1r6s6.codam.nl>
2026-06-03 12:36:12 +0200

    working on building response

--- src/http/HttpResponseBuilder.cpp
+++ src/http/HttpResponseBuilder.cpp
@@ -6,7 +6,7 @@
 /*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 16:15:48 by rmhazres          #+#    #+#             */
-/*   Updated: 2026/06/02 16:48:30 by rmhazres         ###   ########.fr       */
+/*   Updated: 2026/06/03 12:26:39 by rmhazres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@
 HttpResponseBuilder::HttpResponseBuilder() = default;
 HttpResponseBuilder::~HttpResponseBuilder(){};
 
-HttpResponse& HttpResponseBuilder::build()
+HttpResponse HttpResponseBuilder::build(HttpRequest const &request, Server const &server)
 {
 	HttpResponse response;
 
-	
-
+	buildProtocol(request,response);
+	buildStatusCode(request, response,server);
 };
+
+void buildProtocol(const HttpRequest& request,HttpResponse& response)
+{
+	response.setProtocol(request.getProtocol());
+};
+void buildStatusCode(const HttpRequest& request, HttpResponse& response, const Server& server)
+{
+	
+}
--- src/http/HttpResponseBuilder.hpp
+++ src/http/HttpResponseBuilder.hpp
@@ -6,11 +6,13 @@
 /*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 16:15:15 by rmhazres          #+#    #+#             */
-/*   Updated: 2026/06/02 16:32:01 by rmhazres         ###   ########.fr       */
+/*   Updated: 2026/06/03 12:22:50 by rmhazres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../common/HttpResponse.hpp"
+#include "../common/HttpRequest.hpp"
+#include "../Config/Server.hpp"
 
 class HttpResponseBuilder
 {
@@ -19,8 +21,8 @@ class HttpResponseBuilder
 			~HttpResponseBuilder();
 			HttpResponseBuilder(const HttpResponseBuilder &other) = delete;
 			HttpResponseBuilder& operator=(const HttpResponseBuilder &other) = delete;			
-			HttpResponse& build();
-	
+			HttpResponse build(const HttpRequest &request,const Server &server);
 	private:
-	
+			static void buildProtocol(const HttpRequest& request, HttpResponse& response);
+			static void buildStatusCode(const HttpRequest& requets, HttpResponse& response,const Server& Server);
 };
