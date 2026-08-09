4b45b05d72bc49b44eb5d95f17ac0b4a7d0b01ae
Lydia van der Vegt <lydiavdvegt@gmail.com>
2026-08-09 17:26:43 +0200

    created a second web page for the second host, also changed the config file so it will route correctly

---
+++ var2/www/cgi-bin/python/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+import time
+sys.stdout.write("Content-Type: text/html\r\n")
+sys.stdout.write("\r\n")
+sys.stdout.write("<h1>Slow CGI done!</h1>\r\n")
+sys.stdout.flush()
